04f87f84017aeb3432a69bca0a0d4f4d67495357
IU4Nene <sirpot2849@gmail.com>
2023-04-20 00:05:30 +0700

    Add some path and function for product.

--- product_service/urls.py
+++ product_service/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('create', views.create_product),
     path('findByStatus/<str:product_status>', views.get_product_by_status),
     path('findByTags/<str:product_tags>', views.get_product_by_tags),
+    path('<int:product_id>', views.get_product_by_id),
+    path('<int:product_id>/update', views.update_product),
+    path('<int:product_id>/delete', views.delete_product),
 ]
--- product_service/views.py
+++ product_service/views.py
@@ -100,3 +100,70 @@ def get_product_by_tags(request, product_tags):
             }
             product_list.append(product)
         return Response(product_list, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_product_by_id(request, product_id):
+    """Get a product by id."""
+    if request.method == 'GET':
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response({'message': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+        product = {
+            'id': product.id,
+            'name': product.name,
+            'category': {
+                'id': product.category.id,
+                'name': product.category.name
+            },
+            'photo': product.photoUrls,
+            'tags': list(product.tags.all().values('id', 'name')),
+            'status': product.status
+        }
+        return Response(product, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+def update_product(request, product_id):
+    """Update a product."""
+    if request.method == 'PUT':
+        product = request.data
+        product_schema = {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "category": {"type": "string"},
+                "photo": {"type": "array"},
+                "tags": {"type": "array"},
+                "status": {"type": "string"}
+            },
+            "required": ["name", "category", "photo", "tags", "status"]
+        }
+        try:
+            jsonschema.validate(product, product_schema)
+        except jsonschema.exceptions.ValidationError as e:
+            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response({'message': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+        product.name = product['name']
+        product.category = product['category']
+        product.photo = product['photo']
+        product.status = product['status']
+        product.save()
+        product.tags.set(product['tags'])
+        return Response({'message': 'Product updated successfully.'}, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+def delete_product(request, product_id):
+    """Delete a product."""
+    if request.method == 'DELETE':
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response({'message': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+        product.delete()
+        return Response({'message': 'Product deleted successfully.'}, status=status.HTTP_200_OK)
