5331c95aabe7f32817eeb7c8bfec8d647a0835b4
IU4Nene <sirpot2849@gmail.com>
2023-04-22 23:12:55 +0700

    Add user service.

--- user_service/views.py
+++ user_service/views.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.shortcuts import get_object_or_404
 from .models import UserProfile
 from .serializers import UserSerializer
 
