9c2e7ab17d727af0d84ccba3611c63c6285f78a5
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-04-25 00:14:10 +0700

    Add more model to admin

--- product_service/admin.py
+++ product_service/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from django.apps import apps
 
-admin.site.register(Product)
+app = apps.get_app_config('dashboard')
+
+for model_name, model in app.models.items():
+    admin.site.register(model)
