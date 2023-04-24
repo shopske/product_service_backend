26fd54c08e624d45454b25ec9c05394938d20609
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-04-25 00:15:00 +0700

    Change app name

--- product_service/admin.py
+++ product_service/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.apps import apps
 
-app = apps.get_app_config('dashboard')
+app = apps.get_app_config('product_service')
 
 for model_name, model in app.models.items():
     admin.site.register(model)
